f54e22842d3ae1348820f84d6f4be6cdde3098bb
Olga Pidruchna <olgapidruchna@gmail.com>
2022-04-28 12:40:01 -0400

    edited player file and tile file

--- tile.py
+++ tile.py
@@ -1,13 +1,16 @@
 import pygame
 from settings import *
 
+
 class Tile(pygame.sprite.Sprite):
-  """
-  groups: sprite groups it should be a part of
-  """
-  def __init__(self, position, groups):
-    # Initiating the parent Sprite class
-    super().__init__(groups)
-    image = pygame.image.load("pomegranate pixel.png").convert_alpha()
-    self.image = pygame.transform.scale(image, (64, 64))
-    self.rect = self.image.get_rect(topleft = position)
+    """
+    groups: sprite groups it should be a part of
+    """
+
+    def __init__(self, position, groups):
+        # Initiating the parent Sprite class
+        super().__init__(groups)
+        image = pygame.image.load(
+            "/home/opidruchna/the-garden/pomagranate tree.png").convert_alpha()
+        self.image = pygame.transform.scale(image, (64, 64))
+        self.rect = self.image.get_rect(topleft=position)
